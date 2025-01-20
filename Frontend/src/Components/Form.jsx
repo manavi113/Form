42e835478ae3f148c120761855f9b6675667fa21
Manavi Srivastava <srivastavamanavi168@gmail.com>
2025-01-20 04:57:43 -0500

    Frontend

--- Frontend/src/Components/Form.jsx
+++ Frontend/src/Components/Form.jsx
@@ -8,33 +8,10 @@ function Form() {
   const [address, setAddress] = useState('');
   const [age, setAge] = useState('');
 
-  // Handle form submission
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  
+   
+   
 
-    const formData = {
-      name,
-      email,
-      phone,
-      address,
-      age,
-    };
-
-    try {
-      const response = await fetch('http://127.0.0.1:5000/submit', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(formData),
-      });
-
-      const result = await response.json();
-      alert(result.message);
-    } catch (error) {
-      console.error('Error:', error);
-    }
-  };
 
   return (
     <Container maxWidth="sm">
